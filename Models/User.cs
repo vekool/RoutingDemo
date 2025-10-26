6ffb09fddb442a6e43cae3d1fdf69dd5b04c23ae
Vivek Mhatre <vivekrm@gmail.com>
2025-10-26 18:22:10 +0530

    Added Compare Validation for User -> Check in User3 Added Validation in Create3.cshtml

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,6 @@
-﻿namespace RoutingDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoutingDemo.Models
 {
 	public class User
 	{
@@ -6,6 +8,13 @@
 		public string? email { get; set; }
 		public string? address { get; set; }
 		public float? age { get; set; }
+		[Required]
+		[MinLength(5)]
+		public string? Password { get; set; }
+		[Required]
+		[MinLength(5)]
+		[Compare("Password", ErrorMessage ="Passwords do not match")]
+		public string? ComparePassword { get; set; }
 
 		public string? zipcode { get; set; }
 	}
