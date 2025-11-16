efc1e24ab4edb116ac9e22e2e38b584a3f11ed6d
Vivek Mhatre <vivekrm@gmail.com>
2025-11-16 20:08:47 +0530

    OrderDetails Views and Edit (Code for edit not done)

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -18,19 +18,49 @@ namespace RoutingDemo.Controllers
 		public IActionResult Index()
 		{
 			List<AllOrdersVM> data = (from p in ctx.Products
-						join od in ctx.OrderDetails on p.Id equals od.ProductId
-						join o in ctx.Orders on od.OrderId equals o.Id
-						select new AllOrdersVM
-						{
-							Name = p.Name,
-							ODId = od.Id,
-							OID = o.Id,
-							OrderDate = o.OrderDate,
-							Quantity = od.Quantity,
-							Rate= od.Rate
-						}).ToList();
+									  join od in ctx.OrderDetails on p.Id equals od.ProductId
+									  join o in ctx.Orders on od.OrderId equals o.Id
+									  select new AllOrdersVM
+									  {
+										  Name = p.Name,
+										  ODId = od.Id,
+										  OID = o.Id,
+										  OrderDate = o.OrderDate,
+										  Quantity = od.Quantity,
+										  Rate = od.Rate
+									  }).ToList();
 			return View(data);
-						
+		}
+		[HttpGet("{oid?}")]
+		public IActionResult OrderDetailsFor(int? oid)
+		{
+			if (oid == null)
+			{
+				return BadRequest();
+			}
+
+			Order? Order = (from o in ctx.Orders
+						   where o.Id == oid.Value
+						   select o).FirstOrDefault();
+			if (Order == null) {
+				return NotFound();
+			}
+			OrderEditVM ovm = new OrderEditVM();
+			ovm.OrderId = Order.Id;
+			ovm.OrderDate = Order.OrderDate;
+			ovm.OrderDetailRows = (from p in ctx.Products
+								   join od in ctx.OrderDetails
+								   on p.Id equals od.ProductId
+								   where od.OrderId == Order.Id
+								   select new OrderDetailsWithProduct
+								   {
+									   ODId = od.Id,
+									   ProductName = p.Name,
+									   Quantity = od.Quantity,
+									   Rate = od.Rate
+								   }).ToList();
+			
+			return View(ovm);
 		}
 	}
 }
---
+++ Models/ViewModel/OrderEditVM.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoutingDemo.Models.ViewModel
+{
+	public class OrderEditVM
+	{
+		public int OrderId { get; set; }
+		[DataType(DataType.Date)]
+		[Display(Name = "Order Date")]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		public DateTime OrderDate { get; set; }
+
+		public List<OrderDetailsWithProduct> OrderDetailRows { get; set; } = new List<OrderDetailsWithProduct>();
+	}
+	public class OrderDetailsWithProduct
+	{
+		public string ProductName { get; set; } = "";
+		public double Quantity { get; set; }
+
+		public double Rate { get; set; }
+		public int ODId { get; set; }
+	}
+}
